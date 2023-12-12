2181d9ae6095ff236f5463059b0dcfba9522a611
yazanbaker94 <79137633+yazanbaker94@users.noreply.github.com>
2023-12-11 22:53:34 -0700

    added user login/register
    added packages, user login, txt file

--- FinalProject_MovieApp/StartingScreen.cs
+++ FinalProject_MovieApp/StartingScreen.cs
@@ -1,50 +1,111 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 using System.Windows.Forms;
-using Microsoft.VisualBasic;
-using static System.Windows.Forms.VisualStyles.VisualStyleElement.StartPanel;
 
 namespace FinalProject_MovieApp
 {
     public partial class StartingScreen : Form
     {
         private string username = "";
+        private const string filePath = "userCredentials.txt";
+        private bool isReturningUser = false;
+
         public StartingScreen()
         {
             InitializeComponent();
-
-        }
-
-
-        private void startingLabel_Click(object sender, EventArgs e)
-        {
-
         }
 
         private void returningUser_btn_Click(object sender, EventArgs e)
         {
-            label1.Visible = true;
-            textBox.Visible = true;
-
-            label1.Text = "Enter Registered Username: ";
+            isReturningUser = true;
+            ShowInputControls("Enter Registered Username: ");
         }
 
         private void newUser_btn_Click(object sender, EventArgs e)
         {
+            isReturningUser = false;
+            ShowInputControls("Enter New Username: ");
+        }
+
+        private void ShowInputControls(string prompt)
+        {
             label1.Visible = true;
             textBox.Visible = true;
-
-            label1.Text = "Enter New Username: ";
+            label1.Text = prompt;
         }
 
         private void enter_btn_Click(object sender, EventArgs e)
         {
+            string enteredUsername = textBox.Text.Trim();
+
+            try
+            {
+                if (!string.IsNullOrEmpty(enteredUsername))
+                {
+                    if (isReturningUser)
+                    {
+                        if (UserExists(enteredUsername))
+                        {
+                            MessageBox.Show("Login successful!");
+
+                            // Create an instance of Homescreen
+                            Homescreen homeScreen = new Homescreen(); // Correct class name
+
+                            // Close the current form (StartingScreen)
+                            this.Hide();
+
+                            // Show the new form as a dialog
+                            homeScreen.ShowDialog();
+
+                            // Close the application when the Homescreen form is closed
+                            Application.Exit();
+                        }
+                        else
+                        {
+                            MessageBox.Show("User not found. Please register.");
+                        }
+                    }
+                    else
+                    {
+                        RegisterUser(enteredUsername);
+                        MessageBox.Show("User registered successfully!");
+
+                        
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Please enter a username.");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"An error occurred: {ex.Message}");
+            }
+        }
+
+
+
+        private bool UserExists(string usernameToCheck)
+        {
+            string[] lines = File.ReadAllLines(filePath);
+            foreach (string line in lines)
+            {
+                string[] parts = line.Split(':');
+                if (parts.Length == 2 && parts[0].Trim() == usernameToCheck)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        private void RegisterUser(string newUsername)
+        {
+            using (StreamWriter writer = new StreamWriter(filePath, true))
+            {
+                writer.WriteLine($"{newUsername}:password"); // You may want to implement a secure way to store passwords
+            }
         }
     }
 }
