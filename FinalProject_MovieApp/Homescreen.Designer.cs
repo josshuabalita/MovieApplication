1b79d3a2edba75de11662c60ff4775e875fc4166
josshuabalita <joshbalita@icloud.com>
2023-12-15 18:56:40 -0700

    Reviews

--- FinalProject_MovieApp/Homescreen.Designer.cs
+++ FinalProject_MovieApp/Homescreen.Designer.cs
@@ -121,14 +121,14 @@
             // ViewAllMoviesButton
             // 
             this.ViewAllMoviesButton.BackColor = System.Drawing.Color.Black;
-            this.ViewAllMoviesButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ViewAllMoviesButton.Font = new System.Drawing.Font("Sans Serif Collection", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.ViewAllMoviesButton.ForeColor = System.Drawing.Color.Red;
             this.ViewAllMoviesButton.Location = new System.Drawing.Point(1364, 753);
             this.ViewAllMoviesButton.Margin = new System.Windows.Forms.Padding(2);
             this.ViewAllMoviesButton.Name = "ViewAllMoviesButton";
             this.ViewAllMoviesButton.Size = new System.Drawing.Size(239, 58);
             this.ViewAllMoviesButton.TabIndex = 25;
-            this.ViewAllMoviesButton.Text = "View All Movies";
+            this.ViewAllMoviesButton.Text = "Reviews";
             this.ViewAllMoviesButton.UseVisualStyleBackColor = false;
             this.ViewAllMoviesButton.Click += new System.EventHandler(this.ViewAllMoviesButton_Click);
             // 
